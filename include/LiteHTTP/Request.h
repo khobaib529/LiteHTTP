4b69acb6ab0dde52c2647734599108ad09785575
Khobaib <150820696+khobaib529@users.noreply.github.com>
2024-09-23 15:32:28 +0600

    Add placeholder Request class header for LiteHTTP library.

---
+++ include/LiteHTTP/Request.h
@@ -0,0 +1,22 @@
+#ifndef REQUEST_H_
+#define REQUEST_H_
+
+/**
+ * @file Request.h
+ * @brief LiteHTTP Request Class Header
+ *
+ * This header defines the Request class for the LiteHTTP library.
+ * The Request class will represent HTTP requests received by the server.
+ * It is currently a placeholder and will be implemented in future updates.
+ */
+
+namespace LiteHTTP {
+
+class Request {
+ public:
+  // Placeholder for future implementation
+};
+
+}  // namespace LiteHTTP
+
+#endif
